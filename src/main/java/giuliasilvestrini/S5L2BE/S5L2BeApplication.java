c3f93520497a84c71d482e432f94261bc2712a76
Giulia Silvestrini <silvergiuly@hotmail.it>
2024-01-16 14:36:04 +0100

    Initial commit

---
+++ src/main/java/giuliasilvestrini/S5L2BE/S5L2BeApplication.java
@@ -0,0 +1,13 @@
+package giuliasilvestrini.S5L2BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5L2BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S5L2BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/giuliasilvestrini/S5L2BE/S5L2BeApplicationTests.java
@@ -0,0 +1,13 @@
+package giuliasilvestrini.S5L2BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5L2BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
